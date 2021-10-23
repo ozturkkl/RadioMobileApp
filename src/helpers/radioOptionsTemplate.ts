de872946b8ebff33cca69089cdd98f3b39494724
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-10-23 11:09:58 -0400

    Updated Example Radio Options File

--- src/helpers/radioOptionsTemplate.ts
+++ src/helpers/radioOptionsTemplate.ts
@@ -1,17 +1,34 @@
+import { Linking } from "react-native";
+
 export default {
-    RADIO_ICON: require("../assets/images/radio_icon.png"),
     RADIO_NAME: "Radio Name",
-    BACKGROUND_IMG: require("../assets/backgrounds/main_bg1.jpg"),
-    FETCH_ALBUM_COVER_IMAGE: () => {
-        // FETCH ALBUM COVER FROM RADIO API
-        return require('../assets/images/album_cover.jpg')
+    RADIO_ICON: require("../assets/images/radio_icon.png"),
+    RADIO_STREAM_URL: "exampleradio.com/stream/123421",
+    BACKGROUND_IMG: require("../assets/backgrounds/main_bg.jpg"),
+    FETCH_TRACK_INFO: async (setAlbumCover: React.Dispatch<any>, setTrackArtist: React.Dispatch<any>, setTrackName: React.Dispatch<any>) => {
+        let res = await (await fetch("exampleradio.com/track-information/123421")).json()
+        setAlbumCover(res.cover)
+        setTrackArtist(res.artist)
+        setTrackName(res.title)
     },
-    FETCH_TRACK_ARTIST: () => {
-        // FETCH TRACK ARTIST FROM RADIO API
-        return "Example Artist"
-    },
-    FETCH_TRACK_NAME: () => {
-        // FETCH TRACK ARTIST FROM RADIO API
-        return "Example Track Name"
-    }
+    LINKS: [
+        {
+            iconLabel: "youtube",
+            click: () => {
+                Linking.openURL("https://youtube.com/").catch(err => console.error("Couldn't load page", err));
+            }
+        },
+        {
+            iconLabel: "link",
+            click: () => {
+                Linking.openURL("https://google.com").catch(err => console.error("Couldn't load page", err));
+            }
+        },
+        {
+            iconLabel: "mail",
+            click: () => {
+                Linking.openURL("mailto:97kemalozturk@gmail.com").catch(err => console.error("Couldn't load page", err));
+            }
+        },
+    ]
 };
