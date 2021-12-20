8ffac379d5e8acaec18e2f4d88da0bdd70b3b35b
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-12-19 20:33:52 -0500

    Radio randomly playing fix

---
+++ src/helpers/fetchRadioData.ts
@@ -0,0 +1,92 @@
+import radioOptions from '../../radioOptions';
+// import rssParse from '../helpers/rssParse';
+// import {log} from './logger';
+import {getData, setData} from './storage';
+
+export const fetchStreamInfo = async (
+  setAlbumCover: React.Dispatch<any>,
+  setTrackArtist: React.Dispatch<any>,
+  setTrackName: React.Dispatch<any>,
+) => {
+  try {
+    let res = await (
+      await fetch(radioOptions.RADIO_STREAM_TRACK_INFO_URL)
+    ).json();
+    setAlbumCover(res.cover);
+    setTrackArtist(res.artist);
+    setTrackName(res.title);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// export const fetchPodcastsFromCastos = async (
+//   setPodcasts: React.Dispatch<any>,
+// ) => {
+//   const podcasts: object[] = [];
+
+//   const stored = JSON.parse((await getData('podcasts')) || '[]');
+//   const timePassed = Date.now() - Number(await getData('podcasts_updated'));
+//   setPodcasts(stored);
+
+//   // Return chached results if fetched in last 10 mins.
+//   if (timePassed < 60 * 1000 * 10 && timePassed > 0 && stored.length > 0) {
+//     return;
+//   }
+
+//   try {
+//     console.log(
+//       `https://app.castos.com/api/v2/podcasts?token=${radioOptions.CASTOS_TOKEN}`,
+//     );
+//     const list = (
+//       await (
+//         await fetch(
+//           `https://app.castos.com/api/v2/podcasts?token=${radioOptions.CASTOS_TOKEN}`,
+//         )
+//       ).json()
+//     ).data.podcast_list;
+//     for (let item in list) {
+//       console.log(
+//         `https://app.castos.com/api/v2/podcasts/${item}?token=${radioOptions.CASTOS_TOKEN}`,
+//       );
+//       const podcast = (
+//         await (
+//           await fetch(
+//             `https://app.castos.com/api/v2/podcasts/${item}?token=${radioOptions.CASTOS_TOKEN}`,
+//           )
+//         ).json()
+//       ).data;
+
+//       log(await rssParse('https://' + podcast.rss_url));
+//       let {
+//         title,
+//         description,
+//         image: {url},
+//         items,
+//       } = await rssParse('https://' + podcast.rss_url);
+//       const items_obj = items.map(item => {
+//         return {
+//           title: item.title,
+//           url: item.enclosures[0].url,
+//         };
+//       });
+
+//       podcasts.push({
+//         id: item,
+//         title,
+//         description,
+//         imageUrl: url,
+//         items: items_obj,
+//       });
+//     }
+//   } catch (error) {
+//     console.error(
+//       'Error while trying to fetch podcast information from Castos, if you are not using Castos go to fetchRadioData.ts and update the code to fetch from your podcast resource. Error: ' +
+//         error,
+//     );
+//   }
+
+//   setData('podcasts', JSON.stringify(podcasts));
+//   setData('podcasts_updated', Date.now().toString());
+//   setPodcasts(podcasts);
+// };
--- src/helpers/setupPlayer.ts
+++ src/helpers/setupPlayer.ts
@@ -1,22 +1,22 @@
-
-import TrackPlayer, { Capability } from 'react-native-track-player';
-import radio from "../../radioOptions"
+import TrackPlayer, {Capability} from 'react-native-track-player';
+import radio from '../../radioOptions';
 
 TrackPlayer.updateOptions({
-    capabilities: [Capability.Play, Capability.Pause, Capability.Stop,],
-    compactCapabilities: [Capability.Play, Capability.Pause, Capability.Stop],
+  capabilities: [Capability.Play, Capability.Pause, Capability.Stop],
+  compactCapabilities: [Capability.Play, Capability.Pause, Capability.Stop],
 
-    stopWithApp: true,
-    alwaysPauseOnInterruption: true,
+  stopWithApp: true,
+  alwaysPauseOnInterruption: true,
 });
 
 export default async () => {
-    await TrackPlayer.setupPlayer({waitForBuffer: true})
-    await TrackPlayer.reset()
-    await TrackPlayer.add([{
-        title: "",
-        artist: "",
-        url: radio.RADIO_STREAM_URL,
-    }]);
-}
-
+  await TrackPlayer.setupPlayer({waitForBuffer: true});
+  await TrackPlayer.reset();
+  await TrackPlayer.add([
+    {
+      title: '',
+      artist: '',
+      url: radio.RADIO_STREAM_URL,
+    },
+  ]);
+};
