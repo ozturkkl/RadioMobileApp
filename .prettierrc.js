70c4fd44daedcec65a558597c65f3bddcbcc1437
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2022-12-10 04:34:26 -0600

    new react-native initialization

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
   bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: 'avoid',
   printWidth: 150,
   endOfLine: 'auto',
 };
